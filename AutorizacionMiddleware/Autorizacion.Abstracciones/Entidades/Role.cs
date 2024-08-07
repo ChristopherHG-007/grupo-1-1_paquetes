9e5631c2624b7c92f835293c69e4dd06cb5eb31f
Christopher Hernandez <78166548+ChristopherHG-007@users.noreply.github.com>
2024-08-06 20:43:52 -0600

    update Abtracciones

--- AutorizacionMiddleware/Autorizacion.Abstracciones/Entidades/Role.cs
+++ AutorizacionMiddleware/Autorizacion.Abstracciones/Entidades/Role.cs
@@ -10,5 +10,11 @@ namespace Autorizacion.Abstracciones.Entidades
     {
         public int RoleId { get; set; }
         public string RoleName { get; set; }
+        public char? RoleCode { get; set; }
+        public bool RoleStatus { get; set; }
+        //Dates
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdate { get; set; }
+        public DateTime DateDelete { get; set; }
     }
 }
